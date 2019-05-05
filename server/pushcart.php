ddeea2e1cc9796c17731936a1e80709ce65fbffd
How_r_u <kirtrishin@gmail.com>
2019-05-05 23:38:34 +0300

    add some thing

--- server/pushcart.php
+++ server/pushcart.php
@@ -2,8 +2,15 @@
 //print_r($_POST);
 
 include_once("sql_misc.php");
-
+$size = count($_POST);
+if ($size < 4)
+{
+	echo "Заказ не может быть пустым";
+	echo "<br><a href=\"../src/animals.php\">Вернуться в магазин</a>";
+}
+else 
+{
+	echo "Ваш заказ оформлен!";
+	echo "<br><a href=\"../src/animals.php\">Вернуться в магазин</a>";
+}
 sleep(5);
-header("Location: ../src/index.php");
-
-
